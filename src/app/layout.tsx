499fe0312c8ccce478ffa5e8a05f69b22f0b17e8
piyushdhoka <piyushdhoka007@gmail.com>
2025-09-24 13:49:51 +0530

    lalala

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
+import UmamiAnalytics from '@/components/analytics/UmamiAnalytics';
 import ChatBubble from '@/components/common/ChatBubble';
 import Footer from '@/components/common/Footer';
 import Navbar from '@/components/common/Navbar';
 import OnekoCat from '@/components/common/OnekoCat';
 import { Quote } from '@/components/common/Quote';
+import { ThemeProvider } from '@/components/common/ThemeProvider';
 import { generateMetadata as getMetadata } from '@/config/Meta';
 import ReactLenis from 'lenis/react';
 import { ViewTransitions } from 'next-view-transitions';
-import UmamiAnalytics from '@/components/analytics/UmamiAnalytics';
-import Providers from '@/components/common/Providers';
+
 import './globals.css';
 
 export const metadata = getMetadata('/');
@@ -20,19 +21,23 @@ export default function RootLayout({
   return (
     <ViewTransitions>
       <html lang="en" suppressHydrationWarning>
-
         <body className={`font-hanken-grotesk antialiased`}>
-          <Providers>
-          <ReactLenis root>
-            <Navbar />
-            {children}
-            <OnekoCat />
-            <Quote />
-            <Footer />
-            <ChatBubble />
-            <UmamiAnalytics />
-          </ReactLenis>
-          </Providers>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ReactLenis root>
+              <Navbar />
+              {children}
+              <OnekoCat />
+              <Quote />
+              <Footer />
+              <ChatBubble />
+              <UmamiAnalytics />
+            </ReactLenis>
+          </ThemeProvider>
         </body>
       </html>
     </ViewTransitions>
