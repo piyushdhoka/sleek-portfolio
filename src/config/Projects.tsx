88f8b13707fe08dc1b1ba69cb22fd18139cd56fd
piyushdhoka <piyushdhoka007@gmail.com>
2026-01-08 15:28:29 +0530

    movinglines

--- src/config/Projects.tsx
+++ src/config/Projects.tsx
@@ -93,7 +93,29 @@ export const projects: Project[] = [
     projectDetailsPageSlug: '/projects/sweep',
     isWorking: true,
   },
-
+    {
+    title: 'Movinglines',
+    description:
+      'SaaS platform to generate animated videos from natural language prompts, making education content creation accessible.',
+    image: '/project/movinglines.png',
+    video: '',
+    link: 'https://www.movinglines.co.in/',
+    technologies: [
+      { name: 'React', icon: <ReactIcon key="react" /> },
+      { name: 'Node.js', icon: <NodeJs key="nodejs" /> },
+      { name: 'MongoDB', icon: <MongoDB key="mongodb" /> },
+      { name: 'Vercel', icon: <Vercel key="vercel" /> },
+      { name: 'Tailwind CSS', icon: <TailwindCss key="tailwindcss" /> },
+      { name: 'shadcn/ui', icon: <Shadcn key="shadcn" /> },
+      { name: 'Socket.io', icon: <SocketIo key="socketio" /> },
+    ],
+    github: 'https://github.com/piyushdhoka/movinglines',
+    live: 'https://www.movinglines.co.in/',
+    details: true,
+    projectDetailsPageSlug: '/projects/spark_studio',
+    isWorking: true, 
+  },
+  
     {
     title: 'Repo Scout',
     description:
